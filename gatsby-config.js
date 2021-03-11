2668bb33898b22a069472328bd96b8dc74a91e1c
Gehendra Karmacharya <gkarmacharya@deerfieldagency.com>
2021-03-11 13:59:30 -0500

    Xiaflex #1 - Fix display submit button

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: 'Xiaflex Unsubscribe Confirmation',
+    title: "Xiaflex AAHS Registration and Thank You Landing Page",
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
+    "gatsby-plugin-react-helmet",
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -11,29 +11,29 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    'gatsby-plugin-sass',
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
+    "gatsby-plugin-sass",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
-        start_url: '/',
-        background_color: '#ff9900',
-        theme_color: '#ff9900',
-        display: 'minimal-ui',
-        icon: 'src/images/xiaflex-icon.png', // This path is relative to the root of the site.
+        name: "gatsby-starter-default",
+        short_name: "starter",
+        start_url: "/",
+        background_color: "#ff9900",
+        theme_color: "#ff9900",
+        display: "minimal-ui",
+        icon: "src/images/xiaflex-icon.png", // This path is relative to the root of the site.
       },
     },
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: "",
-      }
+      },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
-     'gatsby-plugin-offline'
+    "gatsby-plugin-offline",
   ],
-}
+};
