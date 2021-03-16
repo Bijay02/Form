dda8ad92a8cbed6c285e65a4bdf275fd98dda0fe
Gehendra Karmacharya <gkarmacharya@deerfieldagency.com>
2021-03-16 15:50:33 -0400

    Xiaflex #1 - Updated landing page logo to have withPrefix

--- src/components/dfa-theme/header/index.js
+++ src/components/dfa-theme/header/index.js
@@ -3,6 +3,7 @@ import React from "react";
 import { Grid, Row, Col } from "react-flexbox-grid";
 import SimpleNav from "../../simple-nav";
 import logo from "../../../images/xiaflex-logo.png";
+import { withPrefix } from "gatsby";
 
 const Header = () => (
   <div className="outer-container">
@@ -11,7 +12,7 @@ const Header = () => (
         <Row center="xs">
           <Col xs={12} sm={3} md={3}>
             <Link to="/">
-              <img src={logo} alt="Logo" className="logo" />
+              <img src={withPrefix(logo)} alt="Logo" className="logo" />
             </Link>
           </Col>
           {/* <Col xs={12} sm={9} md={9}>
