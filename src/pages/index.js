54f61a0f3eaabc72fa26dee366e04eff2865cdeb
Mahadev Mandal <mandalmahadev5@gmail.com>
2024-03-21 13:17:41 +0545

    formstack form in iframe

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,32 +3,12 @@ import Layout from "../components/dfa-theme/layout";
 import "./index.scss";
 
 const McqPage = () => {
-  const [formFound, setFormFound] = useState(false);
-  console.log('deploy test')
-  useEffect(() => {
-    let interval;
-    console.log('rendering')
-    if (!formFound) {
-      interval = setInterval(() => {
-        const formStackForm = document.querySelector("#fsform-container");
-        const formContainer = document.querySelector("#form-container");
-
-        if (formStackForm && formContainer) {
-          setFormFound(true);
-          formContainer.appendChild(formStackForm);
-          clearInterval(interval);
-        }
-      }, 200);
-    }
-
-    return () => {
-      clearInterval(interval);
-    };
-  }, [formFound]);
 
   return (
     <Layout>
-      <section id='form-container'>test</section>
+      <section id="form-container">
+        <iframe src="https://deerfieldagency.formstack.com/forms/xdc_hcp_enrollment_form" title="Formstack Form" style={{width:''}}></iframe>
+      </section>
     </Layout>
   );
 };
