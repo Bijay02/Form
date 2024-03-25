cccce3ab89f2c03cb8d2632cdd4827e6b3030bc5
Bijay02 <bijaystha120@gmail.com>
2024-03-25 14:15:38 +0545

    form implement

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,2 +1,20 @@
+import React from "react";
 
-
+export const onRenderBody = ({ setPostBodyComponents }) => {
+  setPostBodyComponents([
+    <>
+      <script
+        type="text/javascript"
+        src="https://deerfieldagency.formstack.com/forms/js.php/xdc_hcp_enrollment_form"
+      ></script>
+      <noscript>
+        <a
+          href="https://deerfieldagency.formstack.com/forms/xdc_hcp_enrollment_form"
+          title="Online Form"
+        >
+          Online Form - XDC HCP Enrollment Form
+        </a>
+      </noscript>
+    </>,
+  ]);
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,35 @@
-import React, { useEffect, useLayoutEffect, useState } from "react";
+import React, { useEffect, useRef } from "react";
 import Layout from "../components/dfa-theme/layout";
 import "./index.scss";
-// import { Col, Row } from "react-flexbox-grid";
 
 const McqPage = () => {
+  const formContainerRef = useRef(null);
+
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src = "https://deerfieldagency.formstack.com/forms/js.php/xdc_hcp_enrollment_form";
+    script.async = true;
+
+    if (formContainerRef.current) {
+      formContainerRef.current.appendChild(script);
+    }
+
+    return () => {
+      if (formContainerRef.current) {
+        formContainerRef.current.removeChild(script);
+      }
+    };
+  }, []);
 
   return (
     <Layout>
-      <section id="form-container">
-        <div className="form-block">
-          <iframe
-            src="https://deerfieldagency.formstack.com/forms/xdc_hcp_enrollment_form"
-            title="Formstack Form"
-            width="100%"
-          ></iframe>
-        </div>
+      <section id="form-container" ref={formContainerRef}>
+        <noscript>
+          <a href="https://deerfieldagency.formstack.com/forms/xdc_hcp_enrollment_form" title="Online Form">
+            Online Form - XDC HCP Enrollment Form
+          </a>
+        </noscript>
       </section>
     </Layout>
   );
